7875697a71155a19239e18416625ee74937a7d78
Jaya Surya Paravada <jayasuryaparavada@gmail.com>
2026-03-14 16:41:29 +0530

    Update dijikstra.js

--- Pathfinding Visualizer/algorithms/dijikstra.js
+++ Pathfinding Visualizer/algorithms/dijikstra.js
@@ -1,18 +1,17 @@
-function runDijkstra() {
+function runDijkstra(){
 
   let dist = {};
   let parent = {};
   let pq = [];
   let visited = new Set();
 
-  // convert coordinates to unique key
   function key(r,c){
-    return r + "," + c;
+    return r + "-" + c;
   }
 
   // initialize distances
-  for(let r=0; r<rows; r++){
-    for(let c=0; c<cols; c++){
+  for(let r=0;r<rows;r++){
+    for(let c=0;c<cols;c++){
       dist[key(r,c)] = Infinity;
     }
   }
@@ -20,7 +19,8 @@ function runDijkstra() {
   dist[key(start[0],start[1])] = 0;
 
   pq.push({
-    pos:start,
+    r:start[0],
+    c:start[1],
     cost:0
   });
 
@@ -28,10 +28,11 @@ function runDijkstra() {
 
     if(pq.length === 0) return;
 
-    // priority queue simulation
     pq.sort((a,b)=>a.cost-b.cost);
 
-    let {pos:[r,c], cost} = pq.shift();
+    let current = pq.shift();
+    let r = current.r;
+    let c = current.c;
 
     if(visited.has(key(r,c))){
       setTimeout(step,10);
@@ -40,20 +41,20 @@ function runDijkstra() {
 
     visited.add(key(r,c));
 
-    // mark visited (skip start/end)
+    // mark visited
     if(!(r===start[0] && c===start[1]) &&
        !(r===end[0] && c===end[1])){
       grid[r][c].classList.add("visited");
     }
 
-    // reached destination
     if(r===end[0] && c===end[1]){
       reconstructPath(parent);
       return;
     }
 
-    // explore neighbors
-    for(let [dr,dc] of [[1,0],[-1,0],[0,1],[0,-1]]){
+    let dirs = [[1,0],[-1,0],[0,1],[0,-1]];
+
+    for(let [dr,dc] of dirs){
 
       let nr = r + dr;
       let nc = c + dc;
@@ -61,7 +62,7 @@ function runDijkstra() {
       if(nr>=0 && nr<rows && nc>=0 && nc<cols &&
          !grid[nr][nc].classList.contains("wall")){
 
-        let newCost = cost + 1;
+        let newCost = dist[key(r,c)] + 1;
 
         if(newCost < dist[key(nr,nc)]){
 
@@ -70,9 +71,11 @@ function runDijkstra() {
           parent[key(nr,nc)] = [r,c];
 
           pq.push({
-            pos:[nr,nc],
+            r:nr,
+            c:nc,
             cost:newCost
           });
+
         }
       }
     }
